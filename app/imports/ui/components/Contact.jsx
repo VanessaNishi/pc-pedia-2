2b66a07c8306bc7bcf9022b8df5812efb3af68ef
Vanessa Nishi <156862214+VanessaNishi@users.noreply.github.com>
2024-04-04 21:42:20 -1000

    Initialize notes-1

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Note from './Note';
+import AddNote from './AddNote';
+
+/** Renders a single row in the List Stuff table. See pages/ListContactsAdmin.jsx. */
+const Contact = ({ contact, notes }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image width={75} src={contact.image} />
+      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <Note key={note._id} note={note} />)}
+      </ListGroup>
+      <AddNote owner={contact.owner} contactId={contact._id} />
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+  notes: PropTypes.arrayOf(PropTypes.shape({
+    note: PropTypes.string,
+    contactId: PropTypes.string,
+    owner: PropTypes.string,
+    createdAt: PropTypes.instanceOf(Date),
+    _id: PropTypes.string,
+  })).isRequired,
+};
+
+export default Contact;
